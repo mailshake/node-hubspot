3d04e168a0cdfbfe30c13a595f8715f4e7fd2002
Geoff Oliver <geoff@mailshake.com>
2024-06-20 14:33:01 -0600

    updated owners endpiont and test

--- test/owners.js
+++ test/owners.js
@@ -3,7 +3,16 @@ const fakeHubspotApi = require('./helpers/fake_hubspot_api')
 const Hubspot = require('..')
 
 describe('Owners', function() {
-  const ownersGetEndpoint = { path: '/owners/v2/owners', response: [] }
+  const ownersGetEndpoint = {
+    path: '/crm/v3/owners',
+    response: {
+      results: [
+        { id: 1, firstName: 'John', lastName: 'Doe' },
+        { id: 2, firstName: 'Jane', lastName: 'Doe' },
+      ],
+    },
+  }
+
   fakeHubspotApi.setupServer({ demo: true, getEndpoints: [ownersGetEndpoint] })
 
   describe('get', function() {
